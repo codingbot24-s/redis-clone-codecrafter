cac1a541b71a68d8ca0d93121a23a7d45cae4089
Saad <primecoder8@gmail.com>
2025-01-30 16:09:35 +0530

    resp parser not working connect issue solved

--- Server.cpp
+++ Server.cpp
@@ -13,74 +13,74 @@
 #include <algorithm>
 
 // parse the incoming Resp message
-class RESPParser
-{
-  // Main Parsing function
-public:
-  static std::vector<std::string> parse_command(const char *buffer, ssize_t length)
-  {
-    std::vector<std::string> parts;
-    size_t pos = 0;
+// class RESPParser
+// {
+//   // Main Parsing function
+// public:
+//   static std::vector<std::string> parse_command(const char *buffer, ssize_t length)
+//   {
+//     std::vector<std::string> parts;
+//     size_t pos = 0;
 
-    if (buffer == "*")
-    {
-      int num_elements = std::stoi(read_until_clrf(buffer, pos));
+//     if (buffer == "*")
+//     {
+//       int num_elements = std::stoi(read_until_clrf(buffer, pos));
 
-      for (int i = 0; i < num_elements; i++)
-      {
-        pos = find_next_part(buffer, pos);
-        int string_length = std::stoi(read_until_clrf(buffer, pos));
+//       for (int i = 0; i < num_elements; i++)
+//       {
+//         pos = find_next_part(buffer, pos);
+//         int string_length = std::stoi(read_until_clrf(buffer, pos));
 
-        std::string part = readStringOfLength(buffer, pos, string_length);
-        parts.push_back(part);
-      }
-    }
-    return parts;
-  }
+//         std::string part = readStringOfLength(buffer, pos, string_length);
+//         parts.push_back(part);
+//       }
+//     }
+//     return parts;
+//   }
 
-  static std::string toUpperCase(const std::string &input)
-  {
-    std::string upper = input;
-    std::transform(upper.begin(), upper.end(), upper.begin(),
-                   [](unsigned char c)
-                   { return std::toupper(c); });
-    return upper;
-  }
+//   static std::string toUpperCase(const std::string &input)
+//   {
+//     std::string upper = input;
+//     std::transform(upper.begin(), upper.end(), upper.begin(),
+//                    [](unsigned char c)
+//                    { return std::toupper(c); });
+//     return upper;
+//   }
 
-  // Helper functions
-private:
-  static std::string read_until_clrf(const char *buffer, size_t &pos)
-  {
-    std::string result;
-    while (buffer[pos] != '\r')
-    {
-      result = result + buffer[pos];
-      pos++;
-    }
-    pos = pos + 2;
-    return result;
-  }
-  static size_t find_next_part(const char *buffer, size_t pos)
-  {
-    while (buffer[pos] != '$')
-    {
-      pos++;
-    }
-    return pos;
-  }
-  // Read a string of spacific length
-  static std::string readStringOfLength(const char *buffer, size_t &pos, int length)
-  {
-    std::string result;
-    pos += 1; // skip $
-    for (int i = 0; i < length; ++i)
-    {
-      result += buffer[pos++];
-    }
-    pos += 2; // skip \r\n
-    return result;
-  }
-};
+//   // Helper functions
+// private:
+//   static std::string read_until_clrf(const char *buffer, size_t &pos)
+//   {
+//     std::string result;
+//     while (buffer[pos] != '\r')
+//     {
+//       result = result + buffer[pos];
+//       pos++;
+//     }
+//     pos = pos + 2;
+//     return result;
+//   }
+//   static size_t find_next_part(const char *buffer, size_t pos)
+//   {
+//     while (buffer[pos] != '$')
+//     {
+//       pos++;
+//     }
+//     return pos;
+//   }
+//   // Read a string of spacific length
+//   static std::string readStringOfLength(const char *buffer, size_t &pos, int length)
+//   {
+//     std::string result;
+//     pos += 1; // skip $
+//     for (int i = 0; i < length; ++i)
+//     {
+//       result += buffer[pos++];
+//     }
+//     pos += 2; // skip \r\n
+//     return result;
+//   }
+// };
 
 // Define a function to interact with client with multiThreading
 
@@ -91,42 +91,26 @@ void interactWithClient(int clientSocket)
   {
     char recv_buff[65536];
     memset(recv_buff, '\0', sizeof(recv_buff));
-
-    ssize_t bytes_received = recv(clientSocket, recv_buff, sizeof(recv_buff), 0);
-
-    if (bytes_received > 0)
+    ssize_t bytes_received = 0;
+    if (bytes_received = recv(clientSocket, recv_buff, sizeof(recv_buff), 0) > 0)
     {
-      std::vector<std::string> command = RESPParser::parse_command(recv_buff, bytes_received);
-
-      if (!command.empty())
-      {
-        std::string cmd = RESPParser::toUpperCase(command[0]);
-
-        if (cmd == "Ping")
-        {
-          std::string Response = "+PONG\r\n";
-          send(clientSocket, Response.c_str(), Response.length(), 0);
-        }
-        else if (cmd == "Echo" && command.size() > 1)
-        {
-          std::string arg = command[1];
-          std::string response = "$" + std::to_string(arg.length()) + "\r\n" + arg + "\r\n";
-          send(clientSocket, response.c_str(), response.length(), 0);
-        }
-      }
+      std::cout << "Received bytes is " << bytes_received << std::endl;
+      std::cout << "Received data is " << recv_buff << std::endl;
+      std::string Response = "+PONG\r\n";
+      send(clientSocket, Response.c_str(), Response.length(), 0);
+      memset(recv_buff, '\0', sizeof(recv_buff));
     }
-    else if (bytes_received == 0)
+    if (bytes_received == 0)
     {
       std::cout << "client disconnected" << std::endl;
       break;
     }
-    else
+    else if (bytes_received == -1)
     {
       std::cerr << "data receiving error" << std::endl;
       break;
     }
   }
-
   close(clientSocket);
 }
 int main(int argc, char **argv)
@@ -153,7 +137,7 @@ int main(int argc, char **argv)
   struct sockaddr_in serverAddress;
   serverAddress.sin_family = AF_INET;
   serverAddress.sin_addr.s_addr = INADDR_ANY;
-  serverAddress.sin_port = htons(6379);
+  serverAddress.sin_port = htons(6378);
   // bind the socket to the port
 
   if (bind(socketServer, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) != 0)
